1e9acdae66d278b8893da02ab18e9780f3941aa3
Arwin Abatayo <arwin.abatayo@techy7.com>
2020-07-27 01:00:07 +0800

    Register fix

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,7 +3,7 @@
 @section('title', __('auth.register'))
 
 @section('content')
-<div class="register-container full-height sm-p-t-30">
+<div class="register-container full-height sm-p-t-40 sm-p-b-40">
     <div class="d-flex justify-content-center flex-column full-height ">
         <img src="{{ asset('pages/assets/img/parcel_bear_logo_h-b.png') }}" alt="logo" data-src="{{ asset('pages/assets/img/parcel_bear_logo_h-b.png') }}" data-src-retina="{{ asset('pages/assets/img/parcel_bear_logo_h-b.png') }}" class="margin-center" width="200" height="89">
         <form id="form-register" class="p-t-15" role="form" method="POST" action="{{ route('register') }}">
@@ -12,7 +12,7 @@
                 <div class="col-md-12">
                     <div class="form-group form-group-default required @error('username') has-error @enderror">
                         <label>{{ __('auth.username') }}</label>
-                        <input type="text" name="username" placeholder="{{ __('auth.enter_field', ['field' => strtolower(__('auth.username'))]) }}" value="{{ old('username') }}" class="form-control" >
+                        <input type="text" name="username" placeholder="{{ __('auth.enter_field', ['field' => strtolower(__('auth.username'))]) }}" value="{{ old('username') }}" class="form-control" required>
                     </div>
                     @error('username')
                     <label class="error" for="username">
@@ -25,7 +25,7 @@
                 <div class="col-md-12">
                     <div class="form-group form-group-default required @error('email') has-error @enderror">
                         <label>{{ __('auth.email') }}</label>
-                        <input type="email" name="email" placeholder="{{ __('auth.enter_field', ['field' => strtolower(__('auth.email'))]) }}" value="{{ old('email') }}" class="form-control" >
+                        <input type="email" name="email" placeholder="{{ __('auth.enter_field', ['field' => strtolower(__('auth.email'))]) }}" value="{{ old('email') }}" class="form-control" required>
                     </div>
                     @error('email')
                     <label class="error" for="email">
@@ -49,7 +49,7 @@
                 <div class="col-md-6">
                     <div class="form-group form-group-default required">
                         <label>{{ __('auth.confirm_password') }}</label>
-                        <input type="password" name="password_confirmation" placeholder="{{ __('auth.enter_field', ['field' => strtolower(__('auth.confirm_password'))]) }}" class="form-control" >
+                        <input type="password" name="password_confirmation" placeholder="{{ __('auth.enter_field', ['field' => strtolower(__('auth.confirm_password'))]) }}" class="form-control" required>
                     </div>
                 </div>
             </div>
@@ -57,7 +57,7 @@
                 <div class="col-md-12">
                     <div class="form-group form-group-default required @error('name') has-error @enderror">
                         <label>{{ __('auth.name') }}</label>
-                        <input type="text" name="name" placeholder="{{ __('auth.enter_field', ['field' => strtolower(__('auth.name'))]) }}" value="{{ old('name') }}" class="form-control" >
+                        <input type="text" name="name" placeholder="{{ __('auth.enter_field', ['field' => strtolower(__('auth.name'))]) }}" value="{{ old('name') }}" class="form-control" required>
                     </div>
                     @error('name')
                     <label class="error" for="name">
@@ -70,7 +70,7 @@
                 <div class="col-md-12">
                     <div class="form-group form-group-default required @error('m_number') has-error @enderror">
                         <label>{{ __('auth.mobile_number') }}</label>
-                        <input type="text" id="m_number" name="m_number" placeholder="{{ __('auth.enter_field', ['field' => strtolower(__('auth.mobile_number'))]) }}" value="{{ old('m_number') }}" class="form-control" >
+                        <input type="text" id="m_number" name="m_number" placeholder="{{ __('auth.enter_field', ['field' => strtolower(__('auth.mobile_number'))]) }}" value="{{ old('m_number') }}" class="form-control" required>
                     </div>
                     @error('m_number')
                     <label class="error" for="m_number">
@@ -83,7 +83,7 @@
                 <div class="col-md-12">
                     <div class="form-group form-group-default required @error('address') has-error @enderror">
                         <label>{{ __('auth.address') }}</label>
-                        <input type="text" name="address" placeholder="{{ __('auth.enter_field', ['field' => strtolower(__('auth.address'))]) }}" value="{{ old('address') }}" class="form-control" >
+                        <input type="text" name="address" placeholder="{{ __('auth.enter_field', ['field' => strtolower(__('auth.address'))]) }}" value="{{ old('address') }}" class="form-control" required>
                     </div>
                     @error('address')
                     <label class="error" for="address">
@@ -96,7 +96,7 @@
                 <div class="col-md-6">
                     <div class="form-group form-group-default @error('postal_code') has-error @enderror">
                         <label>{{ __('auth.postal_code') }}</label>
-                        <input type="text" id="postal_code" name="postal_code" placeholder="{{ __('auth.enter_field', ['field' => strtolower(__('auth.postal_code'))]) }}" value="{{ old('postal_code') }}" class="form-control" >
+                        <input type="text" id="postal_code" name="postal_code" placeholder="{{ __('auth.enter_field', ['field' => strtolower(__('auth.postal_code'))]) }}" value="{{ old('postal_code') }}" class="form-control" required>
                     </div>
                     @error('postal_code')
                     <label class="error" for="postal_code">
@@ -107,7 +107,7 @@
                 <div class="col-md-6">
                     <div class="form-group form-group-default form-group-default-select2 required @error('city') has-error @enderror">
                         <label>{{ __('auth.city') }}</label>
-                        <select name="city" class="full-width" data-placeholder="{{ __('auth.select_field', ['field' => strtolower(__('auth.city'))]) }}" data-init-plugin="select2" >
+                        <select name="city" class="full-width" data-placeholder="{{ __('auth.select_field', ['field' => strtolower(__('auth.city'))]) }}" data-init-plugin="select2" required>
                             <option value=""></option>
                             @foreach (config('location.PH_states_cities') as $state => $cities)
                             <optgroup label="{{ $state }}">
@@ -124,12 +124,12 @@
                 </div>
             </div>
             <div class="row m-t-10 align-items-end">
-                <div class="col-lg-6">
+                <div class="col-md-6">
                     <p><small>{{ __('auth.i_agree_to_the') }} <a href="https://weparcelbear.com/terms-of-use/" class="text-info"><strong>{{ __('auth.terms_of_use') }}</strong></a> & <a href="https://weparcelbear.com/privacy-policy/" class="text-info"><strong>{{ __('auth.privacy_policy') }}</strong></a>.</small></p>
                     <input type="hidden" name="roles" value="3">
                     <button class="btn btn-primary btn-lg btn-cons m-t-10" type="submit">{{ __('auth.create_an_account') }}</button>
                 </div>
-                <div class="col-lg-6 text-right justify-content-end">
+                <div class="col-md-6 text-right justify-content-end">
                     <a href="{{ route('login') }}" class="text-info small"><strong>{{ __('auth.back_to_login') }}</strong></a>
                 </div>
             </div>
--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -7,7 +7,7 @@
     <div class="error-container text-center">
         <h1 class="error-number">{{ __('general.404') }}</h1>
         <h2 class="semi-bold">{{ __('general.404_sorry_but_we_couldnt_find_this_page') }}</h2>
-        <p class="p-b-10">{{ __('general.404_this_page_you_are_looking_for_does_not_exsist') }} <a href="#"><strong>{{ __('general.404_go_back_to_home') }}</strong></a></p>
+        <p class="p-b-10">{{ __('general.404_this_page_you_are_looking_for_does_not_exsist') }} <a href="@if (Auth::check() && auth()->user()->hasAnyRole('Super Admin', 'Admin')) {{ route('admin.dashboard') }} @elseif (Auth::check() && auth()->user()->hasRole('Customer')) {{ route('customer.dashboard', auth()->user()->username) }} @else {{ route('login') }} @endif"><strong>{{ __('general.404_go_back_to_home') }}</strong></a></p>
     </div>
 </div>
 @endsection
