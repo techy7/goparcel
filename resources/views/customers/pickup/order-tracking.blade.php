08031ea5b646b561edf98a8451ae76fd30fd3786
Deeejay24 <david@techy7.com>
2020-08-06 23:44:58 +0800

    Update order-tracking.blade.php

--- resources/views/customers/pickup/order-tracking.blade.php
+++ resources/views/customers/pickup/order-tracking.blade.php
@@ -66,21 +66,7 @@
         </div>
 
         <script src="{{ asset('js/app.js') }}"></script>
-        <script src="{{ asset('pages/assets/plugins/pace/pace.min.js') }}" type="text/javascript"></script>
-        <script src="{{ asset('pages/assets/plugins/liga.js') }}" type="text/javascript"></script>
-        <script src="{{ asset('pages/assets/plugins/jquery/jquery-3.2.1.min.js') }}" type="text/javascript"></script>
-        <script src="{{ asset('pages/assets/plugins/modernizr.custom.js') }}" type="text/javascript"></script>
-        <script src="{{ asset('pages/assets/plugins/jquery-ui/jquery-ui.min.js') }}" type="text/javascript"></script>
-        <script src="{{ asset('pages/assets/plugins/popper/umd/popper.min.js') }}" type="text/javascript"></script>
-        <script src="{{ asset('pages/assets/plugins/bootstrap/js/bootstrap.min.js') }}" type="text/javascript"></script>
-        <script src="{{ asset('pages/assets/plugins/jquery/jquery-easy.js') }}" type="text/javascript"></script>
-        <script src="{{ asset('pages/assets/plugins/jquery-unveil/jquery.unveil.min.js') }}" type="text/javascript"></script>
-        <script src="{{ asset('pages/assets/plugins/jquery-ios-list/jquery.ioslist.min.js') }}" type="text/javascript"></script>
-        <script src="{{ asset('pages/assets/plugins/jquery-actual/jquery.actual.min.js') }}"></script>
-        <script src="{{ asset('pages/assets/plugins/jquery-scrollbar/jquery.scrollbar.min.js') }}"></script>
-        <script src="{{ asset('pages/assets/plugins/classie/classie.js') }}" type="text/javascript"></script>
         <script src="{{ asset('pages/pages/js/pages.js') }}"></script>
-        <script src="{{ asset('pages/assets/js/scripts.js') }}" type="text/javascript"></script>
         <script src="{{ asset('pages/assets/js/scripts.js') }}" type="text/javascript"></script>
     </body>
 </html>
