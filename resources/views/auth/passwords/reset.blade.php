36a450dd131e5a979e95eb05a8b886077695e684
bart-acero <bartacerojr@gmail.com>
2020-10-29 03:16:27 +0800

    Fixed Password Reset

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -15,7 +15,7 @@
           <div class="card-body">
             <form method="POST" action="{{ route('password.update') }}">
               @csrf
-
+              <input type="hidden" name="token" value="{{ $token }}">
               <div class="form-group form-group-default required">
                 <label for="email">{{ __('auth.email') }}</label>
                 <input type="email" id="email" class="form-control required @error('email') has-error @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
